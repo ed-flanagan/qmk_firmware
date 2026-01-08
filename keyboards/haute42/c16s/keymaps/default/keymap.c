e8ca70b6a9c3dc549b058d1ca0293bcd7c2d77e7
Ed Flanagan <ed@flanagan.xyz>
2026-01-07 17:55:12 -0800

    [REMOVE] Debug stuff

--- keyboards/haute42/c16s/keymaps/default/keymap.c
+++ keyboards/haute42/c16s/keymaps/default/keymap.c
@@ -158,6 +158,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // TODO: SOCD
 
 void keyboard_post_init_user(void) {
+    debug_enable=true;
+    debug_matrix=true;
 #ifdef RGB_MATRIX_ENABLE
     rgb_matrix_mode(RGB_MATRIX_CUSTOM_TXKO_SOLID);
 #endif
